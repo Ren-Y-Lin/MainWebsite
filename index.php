1be4cb59eaaaa3874f10bcb775b41cd1a724731c
Ren Lin <renyuan7777@gmail.com>
2021-10-09 04:42:25 -0600

    Improved the CSS of the main page

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
-<?php
-	echo("Hello World");
-?>
+<html>
+	<head>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link href="style.css" rel="stylesheet">
+	</head>
+	<body>
+
+
+
+		<div id="container" class="art">
+			<div id="top-info-box" class="info-box">
+				<div id="image-container">
+				</div>
+				<div id="top-text">
+					<h2>Career Profile</h2>
+					<h3>Software Developer with basic web development experience. A quick learner and problem solver with great teamwork and project manager skills. Expertise includes:</h3>
+				</div>
+			</div>
+			<div id="bottom-info-box" class="info-box">
+				<div id="technical-skills" class="core-info-section">
+				</div>
+				<div id="work-experience" class="core-info-section">
+				</div>
+				<div id="education" class="core-info-section">
+				</div>
+				<div id="languages" class="core-info-section">
+				</div>
+				<div id="other-interests" class="core-info-section">
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
